b6fc498494dad32aea0bdf3eb4b7c8cce293183f
Justin Paulin <hello@justinpaulin.com>
2016-08-20 17:22:30 -0400

    Finished form engine

--- form_engine.py
+++ form_engine.py
@@ -1,14 +1,50 @@
-import sys
+import sys, glob, errno
+import json
+import uuid
 import pdfkit
 
 class form_engine:
+	TEMPLATE_PATH = './templates/'
+	OUTPUT_PATH = './output/'
+	TEMPLATE_FILE_EXTENSION = '.json'
+	OUTPUT_FILE_EXTENSION = '.pdf'
+
 	def getTemplates(self):
-		return False #NOT IMPLEMENTED
+		path = TEMPLATE_PATH + '*' + TEMPLATE_FILE_EXTENSION
+
+		outputDict = {}
+		files = glob.glob(path)
+		for name in files: # 'file' is a builtin type, 'name' is a less-ambiguous variable name.
+    		try:
+        		with open(name) as f: # No need to specify 'r': this is the default.
+            		currentData = json.load(f)
+            		objectDict = {}
+
+            		objectDict['description'] = currentData['description'] # A string
+            		objectDict['keywords'] = currentData['keywords'] # A list
+
+            		outputDict[currentData['title']] = objectDict
+
+    		except IOError as exc:
+        		if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
+            		raise # Propagate other kinds of IOError.
+
+		return outputDict
 
 	def getFields(self, title):
-		return False #NOT IMPLEMENTED
+		fileName = TEMPLATE_PATH + title + TEMPLATE_FILE_EXTENSION
 
-	def generatePdf(self, keys):
+		try:
+        	with open(fileName) as f: # No need to specify 'r': this is the default.
+            	currentData = json.load(f)
+            	fieldDict = currentData['fields']
+
+    	except IOError as exc:
+        	raise # Failed to open template, does it exist?
+
+		return fieldDict
+
+	def generatePdf(self, title, keys):
 		options = {
     		'page-size': 'Letter',
     		'margin-top': '0.75in',
@@ -19,10 +55,23 @@ class form_engine:
     		'no-outline': None,
     		'quiet': ''
 		}
+		fileName = TEMPLATE_PATH + title + TEMPLATE_FILE_EXTENSION
 
-		#pdfkit.from_string('file open template html', 'randomout.pdf', options=options)
+		try:
+        	with open(fileName) as f: # No need to specify 'r': this is the default.
+            	currentData = json.load(f)
 
-		return False #NOT IMPLEMENTED
+            	templateHtml = currentData['html']
+            	for fieldKey in currentData['fields']:
+            		templateHtml.replace('{' + fieldKey + '}', keys[fieldKey])
+
+            	outputFilePath = OUTPUT_PATH + str(uuid.uuid4()) + OUTPUT_FILE_EXTENSION
+            	pdfkit.from_string(templateHtml, outputFilePath, options=options)
+
+    	except:
+        	raise # Failed to open template, does it exist?
+
+		return outputFilePath
 
 if __name__ == "__main__":
 	print "The form engine cannot be run directly. Please run app.py instead."
